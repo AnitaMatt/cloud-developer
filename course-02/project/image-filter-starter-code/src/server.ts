b683caa4253addf9d15f144833400f620737eee7
Anita Matt <arneethar16@yahoo.com>
2022-08-27 22:33:11 +0100

    image-filter

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,6 +1,7 @@
+require('dotenv').config();
 import express from 'express';
 import bodyParser from 'body-parser';
-import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import { filterImageFromURL, deleteLocalFiles } from './util/util';
 
 (async () => {
 
@@ -9,7 +10,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   // Set the network port
   const port = process.env.PORT || 8082;
-  
+
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
@@ -30,17 +31,33 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   /**************************************************************************** */
 
   //! END @TODO1
-  
+  app.get("/filteredimage", async (req, res) => {
+    try {
+      const { image_url } = req.query;
+      if (!image_url) {
+        return res.status(404).json({ message: "image url is required" });
+      }
+      let imagePath = await filterImageFromURL(image_url);
+
+      console.log(imagePath);
+
+      return res.status(200).sendFile(imagePath, () => {
+        deleteLocalFiles([imagePath]);
+      });
+    } catch (error) {
+      return res.status(422).send("ERROR: Image could not br downloaded, Try Again");
+    }
+  });
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
+  app.get("/", async (req,  res) => {
     res.send("try GET /filteredimage?image_url={{}}")
-  } );
-  
+  });
+
 
   // Start the Server
-  app.listen( port, () => {
-      console.log( `server running http://localhost:${ port }` );
-      console.log( `press CTRL+C to stop server` );
-  } );
+  app.listen(port, () => {
+    console.log(`server running http://localhost:${port}`);
+    console.log(`press CTRL+C to stop server`);
+  });
 })();
